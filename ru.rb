fb3395a16d8ec2844be6f832baf229db7d0f0380
Oohira Suguru <suguru.13579515@gmail.com>
2020-08-17 22:21:56 +0900

    Study ruby only sql

--- ru.rb
+++ ru.rb
@@ -1,49 +1,110 @@
+# 参照
 require "./main"
 require './db.rb'
 
+# 変数
 a = 'y'
+time = 1
 
+# 処理内容
 while a == 'y'
 
-  puts "----------------------------"
-  puts "やることを記入してください！"
-  tasks = gets.chomp
-  task = Task.new(task: tasks)
-
-  puts "----------------------------"
-  puts "現在抱えているTasksは以下です"
-  puts "#{task.read}"
-
-  puts "----------------------------"
-  puts "もし削除するタスクがありましたら番号を記入してください。もしもうなければcを記入してください"
-
-  d = gets.chomp.to_i
-
-  unless d.is_a?(Integer)
-    until d.is_a?(Integer)
-      puts "----------------------------"
-      puts "正しい番号を記入してください"
-      puts "----------------------------"
-      puts "もし削除するタスクがありましたら番号を記入してください。もしもうなければcを記入してください"
-      d = gets.chomp.to_i
+  if time > 1
+    # create
+    puts "----------------------------"
+    puts "やることを記入してください！、もしなければn記入してください"
+    tasks = gets.chomp
+    unless tasks == 'n'
+      task = Task.new(task: tasks)
     end
+
+    # read
+    puts "----------------------------"
+    puts "現在抱えているTasksは以下です"
+    puts "#{task.read}"
+
+    # delete
+    puts "----------------------------"
+    puts "もし削除するタスクがありましたら番号を記入してください。もしもうなければcを記入してください"
+
+    d = gets.chomp.to_i
+
+    unless d.is_a?(Integer)
+      until d.is_a?(Integer)
+        puts "----------------------------"
+        puts "正しい番号を記入してください"
+        puts "----------------------------"
+        puts "もし削除するタスクがありましたら番号を記入してください。もしもうなければcを記入してください"
+        d = gets.chomp.to_i
+      end
+    end
+
+    task.delete(d)
+
+    # 確認
+    puts "----------------------------"
+    puts "まだ記入することがありましたらyを記入してください。もしもうなければnを記入してください"
+
+    a = gets.chomp
+
+    unless a == 'y'
+      until a == 'y' || a == 'n'
+        puts "----------------------------"
+        puts "正しい番号を記入してください"
+        puts "----------------------------"
+        puts "まだ記入することがありましたらyを記入してください。もしもうなければnを記入してください"
+        a = gets.chomp
+      end
+    end
+
+  else
+    # create
+    puts "----------------------------"
+    puts "やることを記入してください！"
+    tasks = gets.chomp
+    task = Task.new(task: tasks)
+
+    # read
+    puts "----------------------------"
+    puts "現在抱えているTasksは以下です"
+    puts "#{task.read}"
+
+    # delete
+    puts "----------------------------"
+    puts "もし削除するタスクがありましたら番号を記入してください。もしもうなければcを記入してください"
+
+    d = gets.chomp.to_i
+
+    unless d.is_a?(Integer)
+      until d.is_a?(Integer)
+        puts "----------------------------"
+        puts "正しい番号を記入してください"
+        puts "----------------------------"
+        puts "もし削除するタスクがありましたら番号を記入してください。もしもうなければcを記入してください"
+        d = gets.chomp.to_i
+      end
+    end
+
+    task.delete(d)
+
+    # 確認
+    puts "----------------------------"
+    puts "まだ記入することがありましたらyを記入してください。もしもうなければnを記入してください"
+
+    a = gets.chomp
+
+    unless a == 'y'
+      until a == 'y' || a == 'n'
+        puts "----------------------------"
+        puts "正しい番号を記入してください"
+        puts "----------------------------"
+        puts "まだ記入することがありましたらyを記入してください。もしもうなければnを記入してください"
+        a = gets.chomp
+      end
+    end
+
   end
 
-  task.delete(d)
-
-  puts "----------------------------"
-  puts "まだ記入することがありましたらyを記入してください。もしもうなければnを記入してください"
-
-  a = gets.chomp
-
-  unless a == 'y'
-    until a == 'y' || a == 'n'
-      puts "----------------------------"
-      puts "正しい番号を記入してください"
-      puts "----------------------------"
-      puts "まだ記入することがありましたらyを記入してください。もしもうなければnを記入してください"
-      a = gets.chomp
-    end
-  end
+  time += 1
 
 end
